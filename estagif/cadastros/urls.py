636b6b162fa8734f80ea55c1db4be49a2d839cf3
Andrey <andreybr2002@hotmail.com>
2023-04-24 22:30:38 -0300

    Início do desenvolvimento da listagem e exclusão

--- estagif/cadastros/urls.py
+++ estagif/cadastros/urls.py
@@ -1,11 +1,28 @@
 from django.urls import path
-from .views import MarcaCreate, MarcaUpdate
-from .views import ProdutoCreate, ProdutoUpdate
+from .views import MarcaCreate, MarcaUpdate, MarcaList, MarcaDelete
+from .views import ProdutoCreate, ProdutoUpdate, ProdutoList, ProdutoDelete
+from .views import ClienteCreate, ClienteUpdate, ClienteList, ClienteDelete
 
 urlpatterns = [
     path("cadastrar/marca", MarcaCreate.as_view(), name="cadastrar-marca"),
-    path("atualizar/marca/<int:pk>/", MarcaUpdate.as_view(), name="atualizar-marca"),
+    path("atualizar/marca/<int:pk>/",
+         MarcaUpdate.as_view(), name="atualizar-marca"),
+    path("listar/marca", MarcaList.as_view(), name="listar-marca"),
+    path("excluir/marca/<int:pk>/", MarcaDelete.as_view(), name="excluir-marca"),
+
 
     path("cadastrar/produto", ProdutoCreate.as_view(), name="cadastrar-produto"),
-    path("atualizar/produto/<int:pk>/", ProdutoUpdate.as_view(), name="atualizar-produto"),
+    path("atualizar/produto/<int:pk>/",
+         ProdutoUpdate.as_view(), name="atualizar-produto"),
+    path("listar/produto", ProdutoList.as_view(), name="listar-produto"),
+    path("excluir/produto/<int:pk>/",
+         ProdutoDelete.as_view(), name="excluir-produto"),
+
+
+    path("cadastrar/cliente", ClienteCreate.as_view(), name="cadastrar-cliente"),
+    path("atualizar/cliente/<int:pk>/",
+         ClienteUpdate.as_view(), name="atualizar-cliente"),
+    path("listar/cliente", ClienteList.as_view(), name="listar-cliente"),
+    path("excluir/cliente/<int:pk>/",
+         ClienteDelete.as_view(), name="excluir-cliente"),
 ]
--- estagif/cadastros/views.py
+++ estagif/cadastros/views.py
@@ -1,6 +1,7 @@
 from .models import Marca, Produto, Cliente, Pedido
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # CREATE
@@ -10,14 +11,21 @@ class MarcaCreate(LoginRequiredMixin, CreateView):
     model = Marca
     fields = ["nome"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
+    success_url = reverse_lazy("listar-marca")
 
 
 class ProdutoCreate(LoginRequiredMixin, CreateView):
     model = Produto
     fields = ["nome", "preco", "codigo", "marca"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
+    success_url = reverse_lazy("listar-produto")
+
+
+class ClienteCreate(LoginRequiredMixin, CreateView):
+    model = Cliente
+    fields = ["nome", "cpf", "telefone"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-cliente")
 
 
 # UPDATE
@@ -27,11 +35,57 @@ class MarcaUpdate(LoginRequiredMixin, UpdateView):
     model = Marca
     fields = ["nome"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
+    success_url = reverse_lazy("listar-marca")
 
 
 class ProdutoUpdate(LoginRequiredMixin, UpdateView):
     model = Produto
     fields = ["nome", "preco", "codigo", "marca"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
+    success_url = reverse_lazy("listar-produto")
+
+
+class ClienteUpdate(LoginRequiredMixin, UpdateView):
+    model = Cliente
+    fields = ["nome", "cpf", "telefone"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-cliente")
+
+
+# LIST
+
+
+class MarcaList(ListView):
+    model = Marca
+    template_name = "cadastros/list/marca.html"
+
+
+class ProdutoList(ListView):
+    model = Produto
+    template_name = "cadastros/list/.html"
+
+
+class ClienteList(ListView):
+    model = Cliente
+    template_name = "cadastros/list/.html"
+
+
+# DELETE
+
+
+class MarcaDelete(DeleteView):
+    model = Marca
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-marca")
+
+
+class ProdutoDelete(DeleteView):
+    model = Produto
+    template_name = "cadastros/list/delete.html"
+    success_url = reverse_lazy("listar-produto")
+
+
+class ClienteDelete(DeleteView):
+    model = Cliente
+    template_name = "cadastros/list/delete.html"
+    success_url = reverse_lazy("listar-cliente")
