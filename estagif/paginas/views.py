1fafe506a372e749e03bfa7d9da88b5a432a5532
Andrey <andreybr2002@hotmail.com>
2023-08-21 20:16:31 -0300

    Relatório tela inicial

--- estagif/paginas/views.py
+++ estagif/paginas/views.py
@@ -1,9 +1,19 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from cadastros.models import Cliente, Pedido, Produto
+from datetime import datetime
 
 class InicioView(LoginRequiredMixin, TemplateView):
     template_name = "paginas/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["clientes"] = Cliente.objects.filter(criado_em__month=datetime.now().month).count()
+        context["pedidos"] = Pedido.objects.filter(criado_em__month=datetime.now().month).count()
+        context["produtos"] = Produto.objects.filter(criado_em__month=datetime.now().month).count()
+
+        return context
 
 
 class SobreView(LoginRequiredMixin, TemplateView):
